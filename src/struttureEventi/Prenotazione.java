1af9193a0911ebf3d9e5508556b3ab684f614579
Jessica Cinelli <jessicacinelli@live.com>
2020-11-27 20:02:04 +0100

    Aggiornamento e caricamento UI prenotazioni

--- src/struttureEventi/Prenotazione.java
+++ src/struttureEventi/Prenotazione.java
@@ -1,25 +1,28 @@
 package struttureEventi;
 
-
+import contabilità.Cliente;
 
 public class Prenotazione {
 	
-	
-	
-	public Prenotazione(String idPrenotazione) {
-		super();
+	public Prenotazione(String idPrenotazione, Cliente cliente) {
 		this.idPrenotazione = idPrenotazione;
-		
+		this.cliente = cliente;
 	}
-
+	
 	public String getIdPrenotazione() {
 		return idPrenotazione;
 	}
-
 	public void setIdPrenotazione(String idPrenotazione) {
 		this.idPrenotazione = idPrenotazione;
 	}
+	public Cliente getCliente() {
+		return cliente;
+	}
+	public void setCliente(Cliente cliente) {
+		this.cliente = cliente;
+	}
 
 	protected String idPrenotazione;
+	public Cliente cliente;
 	
 }
--- src/struttureEventi/PrenotazioneRistorante.java
+++ src/struttureEventi/PrenotazioneRistorante.java
@@ -1,11 +1,11 @@
 package struttureEventi;
 
-
+import contabilità.Cliente;
 
 public class PrenotazioneRistorante extends Prenotazione{
 
-	public PrenotazioneRistorante(String idPrenotazione, int nTavolo) {
-		super(idPrenotazione);
+	public PrenotazioneRistorante(String idPrenotazione, Cliente cliente, int nTavolo) {
+		super(idPrenotazione, cliente);
 		this.nTavolo = nTavolo;
 	}
 
