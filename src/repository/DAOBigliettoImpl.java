4509bbad0cb633f51a0af9282dd8955d3ed919e0
Christian <christian_ve98@hotmail.it>
2020-12-13 11:29:24 +0100

    fix database, fix DAOBigliettoImpl

--- src/repository/DAOBigliettoImpl.java
+++ src/repository/DAOBigliettoImpl.java
@@ -31,7 +31,7 @@ public class DAOBigliettoImpl implements DAOBiglietto {
 			while (result.next()) {
 				String id = result.getString("IdBiglietto");
 				float costo=result.getFloat("Costo");
-				boolean disponibilità=result.getBoolean("Disponibilità");
+				boolean disponibilità=result.getBoolean("Disponibilita");
 				String evento=result.getString("NomeEvento");
 			
 				Biglietto b = new Biglietto(id, costo, disponibilità, evento);
@@ -55,7 +55,7 @@ public class DAOBigliettoImpl implements DAOBiglietto {
 			while (result.next()) {
 				String idBiglietto = result.getString("IdBiglietto");
 				float costo=result.getFloat("Costo");
-				boolean disponibilità=result.getBoolean("Disponibilità");
+				boolean disponibilità=result.getBoolean("Disponibilita");
 				String evento=result.getString("NomeEvento");
 				b= new Biglietto(idBiglietto, costo, disponibilità, evento);
 			
