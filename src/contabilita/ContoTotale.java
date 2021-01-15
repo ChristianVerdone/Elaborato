557f27418a259d2574dc74918c4116089cd718b2
Jessica Cinelli <jessicacinelli@live.com>
2021-01-15 16:26:14 +0100

    Update VoceConto

--- src/contabilita/ContoTotale.java
+++ src/contabilita/ContoTotale.java
@@ -1,6 +1,7 @@
 package contabilita;
 
 import java.time.LocalDate;
+import java.util.ArrayList;
 
 public class ContoTotale {
 
@@ -9,16 +10,26 @@ public class ContoTotale {
 		this.importo = importo;
 		this.dataPagamento = dataPagamento;
 		this.codCliente = codCliente;
-	}
-
-	public String getNumeroConto() {
-		return numeroConto;
+		this.voci=voci;
 	}
 
 	@Override
 	public String toString() {
 		return "ContoTotale [numeroConto=" + numeroConto + ", importo=" + importo + ", dataPagamento=" + dataPagamento
 				+ ", codCliente=" + codCliente + "]";
+	}
+
+	public String getNumeroConto() {
+		return numeroConto;
+	}
+
+	
+	public String getCodCliente() {
+		return codCliente;
+	}
+
+	public ArrayList<VoceConto> getVoci() {
+		return voci;
 	}
 
 	public double getImporto() {
@@ -32,9 +43,22 @@ public class ContoTotale {
 	public String getCliente() {
 		return codCliente;
 	}
-
+	
+	public void addVoce(VoceConto vc) {
+		voci.add(vc);
+	}
+	public void printConto() {
+		System.out.println(numeroConto);
+		System.out.println(importo);
+		System.out.println(dataPagamento);
+		System.out.println(codCliente);
+		for(VoceConto v: voci) {
+			System.out.println(v);
+		}
+	}
 	private String numeroConto;
 	private double importo;
 	private LocalDate dataPagamento;
 	private String codCliente;
+	private ArrayList<VoceConto> voci;
 }
