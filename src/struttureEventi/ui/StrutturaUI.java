350ba653ceda3f16b8ad16fc37ba1ad2b1015f7d
Christian <christian_ve98@hotmail.it>
2021-02-03 15:39:17 +0100

    Fix Clienti in Prenotazione struttura (Solo clienti prenotati)

--- src/struttureEventi/ui/StrutturaUI.java
+++ src/struttureEventi/ui/StrutturaUI.java
@@ -31,9 +31,8 @@ import struttureEventi.classes.PrenotazioneSv;
 import struttureEventi.classes.Tessera;
 import util.GenerateRandom;
 
-
 public class StrutturaUI extends JFrame implements ActionListener {
-	
+
 	private ArrayList<Tessera> tessere;
 	private ArrayList<Cliente> clienti;
 	private JFrame frame;
@@ -44,6 +43,7 @@ public class StrutturaUI extends JFrame implements ActionListener {
 	private ArrayList<Evento> eventi;
 	private Evento evento;
 	private int c, t;
+
 	/**
 	 * Launch the application.
 	 */
@@ -59,7 +59,7 @@ public class StrutturaUI extends JFrame implements ActionListener {
 			}
 		});
 	}
-	
+
 	public void start() {
 		EventQueue.invokeLater(new Runnable() {
 			public void run() {
@@ -77,141 +77,125 @@ public class StrutturaUI extends JFrame implements ActionListener {
 	 * Create the application.
 	 */
 	public StrutturaUI() {
-		
-		
+
 		frame = new JFrame();
 		frame.setBounds(100, 100, 488, 345);
 		frame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
 		frame.setTitle("Prenotazione Struttura");
-		
+
 		JLabel lbl_logo = new JLabel();
 		lbl_logo.setLocation(119, 22);
 		lbl_logo.setSize(231, 80);
 		lbl_logo.setIcon(new ImageIcon("res/logo.png"));
 		lbl_logo.setBackground(Color.DARK_GRAY);
 		frame.getContentPane().add(lbl_logo);
-		
-		String[] strutture = {"Campo da tennis", "Campo da calcio"};
-		
+
+		String[] strutture = { "Campo da tennis", "Campo da calcio" };
+
 		JComboBox struttbox = new JComboBox(strutture);
-		
+
 		struttbox.setBounds(150, 122, 131, 22);
 		frame.getContentPane().add(struttbox);
-		
-		
+
 		JLabel lblNewLabel_1 = new JLabel("Seleziona la struttura");
 		lblNewLabel_1.setBounds(9, 122, 131, 14);
 		frame.getContentPane().add(lblNewLabel_1);
-		
-		
-		
-		
+
 		JLabel lblNewLabel_2 = new JLabel("Seleziona il cliente");
 		lblNewLabel_2.setBounds(9, 166, 112, 14);
 		frame.getContentPane().add(lblNewLabel_2);
-		
-		
+
 		JButton btnNewButton = new JButton("Prenota");
 		btnNewButton.setBounds(186, 279, 99, 23);
 		btnNewButton.setActionCommand("prenota");
 		btnNewButton.addActionListener(this);
 		frame.getContentPane().setLayout(null);
 		frame.getContentPane().add(btnNewButton);
-		/**CLIENTI*/
+		/** CLIENTI */
 		clienti = new ArrayList<Cliente>();
-		for (Cliente cl : DAOFactory.getDAOCliente().doRetrieveAll().values()) {
+		for (Cliente cl : DAOFactory.getDAOCliente().doRetrieveClientiPrenotati().values()) {
 			clienti.add(cl);
 		}
 		DefaultTableModel dtm = new DefaultTableModel() {
-		    @Override
-		    public boolean isCellEditable(int row, int column) {
-		        return false;
-		    }
+			@Override
+			public boolean isCellEditable(int row, int column) {
+				return false;
+			}
 		};
-		dtm.setColumnIdentifiers(new String[]{"Nome","Cognome","CF"});
-		
-		//Da spostare in un controller
-		Set<Cliente> items = new HashSet<Cliente>(); 
-		items.addAll(clienti);
-		
-		for(Cliente cl : clienti) {
-			dtm.addRow(new Object[]{cl.getNome(), cl.getCognome(),  cl.getCf()});
+		dtm.setColumnIdentifiers(new String[] { "Nome", "Cognome", "CF" });
+
+		for (Cliente cl : clienti) {
+			dtm.addRow(new Object[] { cl.getNome(), cl.getCognome(), cl.getCf() });
 		}
-		
+
 		table = new JTable();
 		table.setBounds(344, 322, 314, -206);
 		table.setModel(dtm);
 		table.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
-		//table.getSelectionModel().addListSelectionListener(this);
-	
+		// table.getSelectionModel().addListSelectionListener(this);
+
 		JScrollPane scrollPane_table_1 = new JScrollPane(table);
 		scrollPane_table_1.setBounds(150, 165, 309, 89);
 		frame.getContentPane().add(scrollPane_table_1);
 
-		/**TESSERE*/
-		
+		/** TESSERE */
 		DefaultTableModel dtmT = new DefaultTableModel() {
-		    @Override
-		    public boolean isCellEditable(int row, int column) {
-		        return false;
-		    }
+			@Override
+			public boolean isCellEditable(int row, int column) {
+				return false;
+			}
 		};
-		dtmT.setColumnIdentifiers(new String[]{"Tessera","Descrizione"});
-	
+		dtmT.setColumnIdentifiers(new String[] { "Tessera", "Descrizione" });
+
 		struttbox.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent arg0) {
-				struttura=(String) struttbox.getSelectedItem();
-				
-			
-			}}
-		);
-		
+				struttura = (String) struttbox.getSelectedItem();
+			}
+		});
 	}
-		
-			
-	
-	public  void actionPerformed(ActionEvent e) {
 
-		
-	
-	String command= e.getActionCommand();
-	
-	switch (command) {
-	case "prenota":
-		 c =table.getSelectedRow();
-		 
-		
-		if (struttura==null) { 
-			JOptionPane.showMessageDialog(this, "Selezionare la struttura.");
-			break;}
-	
-		if(c==-1) {
-			JOptionPane.showMessageDialog(this, "Selezionare un cliente.");
-			break;}
-		
-		cliente=clienti.get(c);
-		
-		GenerateRandom g = new GenerateRandom();
-		String idPrenotazione = "PE" + g.GenerateRandom();
-		String idTessera= "TS" + g.GenerateRandom();
-		Tessera t= new Tessera(idTessera, "Tessera per il " + struttura.toLowerCase());
-		System.out.println(t.toString());
-		PrenotazioneSv p= new PrenotazioneSv(idPrenotazione, cliente, DAOFactory.getDAOStrutturaVillaggio().doRetrieveById(struttura), t);
-		System.out.println(DAOFactory.getDAOStrutturaVillaggio().doRetrieveById(struttura));
-		int prenotazione=DAOFactory.getDAOPrenotazioneStruttura().updatePrenotazioneStruttura(p);
-		this.dispose();
-		frame.dispose();
-		if(prenotazione!= 0) 
-			JOptionPane.showMessageDialog(this, "Prenotazione effettuata!");
-		else if(prenotazione==0)
-			JOptionPane.showMessageDialog(this, "Errore durante la registrazione della prenotazione!");
-			
-		else if(DAOFactory.getDAOPrenotazioneStruttura().doRetrieveById(idPrenotazione)!= null) {
-			JOptionPane.showMessageDialog(this, "Non è possibile registrare la prenotazione.\nE' già presente una prenotazione con identificativo " + idPrenotazione);
+	public void actionPerformed(ActionEvent e) {
+
+		String command = e.getActionCommand();
+		switch (command) {
+		case "prenota":
+			c = table.getSelectedRow();
+
+			if (struttura == null) {
+				JOptionPane.showMessageDialog(this, "Selezionare la struttura.");
+				break;
+			}
+
+			if (c == -1) {
+				JOptionPane.showMessageDialog(this, "Selezionare un cliente.");
+				break;
+			}
+
+			cliente = clienti.get(c);
+			GenerateRandom g = new GenerateRandom();
+			String idPrenotazione = "PE" + g.GenerateRandom();
+			String idTessera = "TS" + g.GenerateRandom();
+			Tessera t = new Tessera(idTessera, "Tessera per il " + struttura.toLowerCase());
+			System.out.println(t.toString());
+			PrenotazioneSv p = new PrenotazioneSv(idPrenotazione, cliente,
+					DAOFactory.getDAOStrutturaVillaggio().doRetrieveById(struttura), t);
+			System.out.println(DAOFactory.getDAOStrutturaVillaggio().doRetrieveById(struttura));
+			int prenotazione = DAOFactory.getDAOPrenotazioneStruttura().updatePrenotazioneStruttura(p);
+			this.dispose();
+			frame.dispose();
+			if (prenotazione != 0)
+				JOptionPane.showMessageDialog(this, "Prenotazione effettuata!");
+			else if (prenotazione == 0)
+				JOptionPane.showMessageDialog(this, "Errore durante la registrazione della prenotazione!");
+
+			else if (DAOFactory.getDAOPrenotazioneStruttura().doRetrieveById(idPrenotazione) != null) {
+				JOptionPane.showMessageDialog(this,
+						"Non è possibile registrare la prenotazione.\nE' già presente una prenotazione con identificativo "
+								+ idPrenotazione);
+			}
+			this.dispose();
+			frame.dispose();
+			break;
 		}
-		this.dispose();
-		frame.dispose();
-		break;
-	
 	}
-	}}
+}
